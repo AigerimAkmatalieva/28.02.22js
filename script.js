ef14ba1b37f45ca8371da0366661e669a5f1c1e2
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-28 15:05:18 +0600

    added function

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ let fruits = ["apple", "banana", "giwi", "peach"];
 fruits.forEach((item) => {
   console.log(item)
 });
+
+
+let word = ["apple", "banana",
+ false, 13,
+"hello", 
+["1", "2", "3"], 
+{ name: "Peter", 
+Lastname:"Parker" },
+ 12345];
+
+word.forEach((item) => {
+  console.log(typeof(item))
+});
+
+
+
